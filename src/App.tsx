1b3a507cdccef3066d3bd397856279680db78b7e
glayson <glaysonwow@gmail.com>
2021-07-11 19:15:34 -0300

    Finalização da aula 04

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
+import { AdminRoom } from "./pages/AdminRoom";
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
@@ -22,4 +24,4 @@ function App() {
 
 export default App;
 
-//Aula 3 parou em 01:01:41.
+//Aula 5 00:00:00.
